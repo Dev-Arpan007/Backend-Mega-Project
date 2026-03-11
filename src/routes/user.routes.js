0f9f4616935d84ae2637c2e31975ec46a6cf5357
Dev-Arpan007 <chakrabortyarpan2004@gmail.com>
2026-03-11 12:43:46 +0530

    Refactor user routes to include account management features: change password, update details, and avatar/cover image upload

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
-import { logoutUser, registerUser, loginUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    logoutUser, 
+    registerUser, 
+    loginUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from "../controllers/user.controller.js";
+
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -29,4 +42,21 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh-token").post( refreshAccessToken )
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails)
+
+router.route("/change-avatar").patch(verifyJWT, 
+  upload.single("avatar"),  updateUserAvatar)
+
+router.route("/change-cover-image").patch(verifyJWT, 
+  upload.single("coverImage"),  updateUserCoverImage)
+
+router.route("/channel/:userName").get(verifyJWT, getUserChannelProfile) //since we are taking userName from url (params), we need the url to be defined like this
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
+  
 export default router
