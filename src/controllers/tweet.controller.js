24e03b1de409b9605875a3e1afdb5cdd878fc36a
Dev-Arpan007 <chakrabortyarpan2004@gmail.com>
2026-04-14 17:52:26 +0530

    Implement tweet CRUD operations with error handling to add functionalities of tweet.controller.js and add tweet routes

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,24 +1,118 @@
 import mongoose, { isValidObjectId } from "mongoose"
 import {Tweet} from "../models/tweet.model.js"
 import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
+import {ApiError} from "../utils/ApiErrors.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+   
+     const user = req?.user._id
+     if (!isValidObjectId(user)) {
+        throw new ApiError(400, "Invalid userId")
+     }
+     const content = req.body.content
+     console.log("hhhhhhhhhh")
+     if(!content || content?.trim() === ""){
+        console.log("vvvvvvvvv")
+         throw new ApiError(400, "Tweet content missing")
+        
+     }
+ 
+     const tweet = await Tweet.create({
+         "content": content,
+         "owner" : user
+     })
+ 
+ 
+     return res
+     .status(201)
+     .json(new ApiResponse(201, tweet, "Tweet properly posted"))
+  
+
+    
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const {userId} = req.params
+
+    if(!userId){
+        throw new ApiError(404, "UserId not found")
+    }
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid userId")
+    }
+    const userTweets = await Tweet.find({
+        owner: userId
+    }).sort({createdAt: -1});
+
+    return res.
+    status(200)
+    .json(
+        new ApiResponse(200, userTweets, "All tweets of this user fetched successfully")
+    )
+
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const user = req?.user._id
+    const {tweetId} = req.params
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweetId")
+    }
+    const {updatedContent} = req.body
+    if (!updatedContent) {
+        throw new ApiError(400, "Updated content missing")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet){
+        throw new ApiError(404, "Tweet not found")
+    }
+    if(tweet.owner.equals(user) ){
+        tweet.content = updatedContent
+        await tweet.save({validateBeforeSave: false})
+    }else{
+        throw new ApiError(403, "Not a owner to update Tweet")
+    }
+
+    const updatedTweet = await Tweet.findById(tweet._id)
+    
+    if(updatedTweet?.content !== updatedContent){
+        throw new ApiError(401, "Tweet could not be updated")
+    }
+    return res.
+    status(200)
+    .json(new ApiResponse(200,updatedTweet, "Tweet Updated Successfully" ))
+
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const user = req?.user._id
+    const {tweetId} = req.params
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweetId")
+    }
+    const delTweet = await Tweet.findById(tweetId)
+    if(!delTweet){
+        throw new ApiError(404, "Tweet not found")
+    }
+    if(!delTweet.owner.equals(user)){
+        throw new ApiError(403, "Not a owner to delete tweet")
+    }
+
+    await delTweet.deleteOne()
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "tweet sucessfully deleted"))
+
+
 })
 
 export {
---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { createTweet, getUserTweets, deleteTweet, updateTweet } from "../controllers/tweet.controller.js";
+
+const router = Router()
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router.route('/create-tweet').post(createTweet)
+router.route("/get-tweets/:userId").get(getUserTweets)
+router.route("/update-tweet/:tweetId").patch(updateTweet)
+router.route("/delete-tweet/:tweetId").delete(deleteTweet)
+
+
+export default router
+
